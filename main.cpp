7e90056d2d05d7a5367df0b53b70756fac6b47d3
pixelman77 <haghgoo33@gmail.com>
2025-09-21 00:39:41 +0330

    more work on fundementals

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <string>
 
-
 enum ProjectionMode
 {
     PROJ_PERSPECTIVE,
@@ -21,10 +20,97 @@ enum RenderMode
     RENDER_ZBUFFER_BUFFER
 };
 
-int SCREEN_WIDTH = 800;
-int SCREEN_HEIGHT = 600;
+
+//buffer input gets a seizure with these and I have no idea why
+uint32_t SCREEN_WIDTH = 800;
+uint32_t SCREEN_HEIGHT = 600;
 float HALFWIDTH = SCREEN_WIDTH / 2;
 float HALFHEIGHT = SCREEN_HEIGHT / 2;
+
+
+struct ScreenBuffer
+{
+    // note to self: w of h/x of y
+    // down growing
+    std::vector<uint32_t> buffer;
+    int width, height;
+    float halfwidth, halfheight;
+
+    ScreenBuffer(uint32_t iwidth, uint32_t iheigth){
+
+        this->width = iwidth;
+        this->height = iheigth;
+        halfheight = height / 2;
+        halfwidth = width / 2;
+        buffer = std::vector<uint32_t>(width * height, 0);
+    }
+
+    void fill(uint32_t color){
+        std::fill(buffer.begin(), buffer.end(), color);
+    }
+
+    uint32_t get(int x, int y){
+        if(x >= width || y>=height) return 0;
+        return buffer[y * width + x];
+    }
+
+    void set(int x, int y, uint32_t color){
+        if(x < 0 || y < 0 || x >= width || y>=height) return;
+        buffer[y * width + x] = color;
+    }
+
+    void line( int x0, int y0, int x1, int y1, uint32_t color) {
+        int dx = std::abs(x1 - x0);
+        int dy = std::abs(y1 - y0);
+        int sx = (x0 < x1) ? 1 : -1;
+        int sy = (y0 < y1) ? 1 : -1;
+        int err = dx - dy;
+
+        while (true) {
+            set(x0, y0, color); // draw the pixel
+
+            if (x0 == x1 && y0 == y1) break;
+
+            int e2 = 2 * err;
+            if (e2 > -dy) {
+                err -= dy;
+                x0 += sx;
+            }
+            if (e2 < dx) {
+                err += dx;
+                y0 += sy;
+            }
+        }
+    }
+
+//this following code only draws vertexes (either from one side or wherever), will I ever investigate?
+/*
+    void line(int x1, int y1, int x2, int y2, uint32_t color){
+        //shamelessly stolen from tinyRenderer
+        bool steep = std::abs(x1-x2) < std::abs(y1-y2);
+        if (steep) { // if the line is steep, we transpose the image
+            std::swap(x1, y1);
+            std::swap(x2, y2);
+        }
+        if (x1>x2) { // make it left−to−right
+            std::swap(x1, x2);
+            std::swap(y1, y2);
+        }
+        int y = y1;
+        int ierror = 0;
+        for (int x=x1; x<=x1; x++) {
+            if (steep) // if transposed, de−transpose
+                set(y, x, color);
+            else
+                set(x, y, color);
+            ierror += 2 * std::abs(y2-y1);
+            y += (y2 > y1 ? 1 : -1) * (ierror > x2 - x1);
+            ierror -= 2 * (x2-x1)   * (ierror > x2 - x1);
+        } 
+
+    }
+    */
+};
 
 struct Vector2d
 {
@@ -186,32 +272,37 @@ Vector3d rotatePoint(const Vector3d& point, const Vector3d& pivot, const Vector3
 }
 
 
-Vector2di orthos(Vector3d point, Camera &camera){
+Vector2di orthos(Vector3d point, Camera &camera, ScreenBuffer &screenBuffer){
     
-    Vector3d rotatedPoint = rotatePoint(point, camera.position, camera.rotation);
     Vector2di screenPoint;
+    Vector3d relative = point - camera.position;
+    Vector3d rotatedPoint = rotatePoint(relative, Vector3d(0,0,0), Vector3d(-camera.rotation.x, -camera.rotation.y, -camera.rotation.z));
 
-    screenPoint.x = rotatedPoint.x * camera.zoom * camera.aspect + HALFWIDTH; 
-    screenPoint.y = -rotatedPoint.y * camera.zoom + HALFHEIGHT;
+    screenPoint.x = rotatedPoint.x * camera.zoom * camera.aspect + screenBuffer.halfwidth; 
+    screenPoint.y = -rotatedPoint.y * camera.zoom + screenBuffer.halfheight;
 
     return screenPoint;
 }
 
 int main() {
 
+
+    ScreenBuffer screenBuffer(800, 600);
     std::vector<Triangle> tris = loadTRIS("utah_teapot.tris");
     Camera mainCamera;
+    mainCamera.aspect = screenBuffer.width / screenBuffer.height;
+    mainCamera.zoom = 90.0f;
+    mainCamera.position.y = 2;
+    
 
     SDL_Window* window = SDL_CreateWindow("Hello SDL3",
-                                          SCREEN_WIDTH, SCREEN_HEIGHT,
-                                          SDL_WINDOW_VULKAN);
+                                          SCREEN_WIDTH, SCREEN_HEIGHT, 0);
     if (!window) {
         std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << "\n";
         SDL_Quit();
         return 1;
     }
 
-    // Create a renderer so we can draw things
     SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
     if (!renderer) {
         std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << "\n";
@@ -219,17 +310,15 @@ int main() {
         SDL_Quit();
         return 1;
     }
+    SDL_Texture* framebuffer = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        screenBuffer.width, screenBuffer.height);
 
-    //little testing bit 
-    for(Triangle t : tris){
-        t.v1.z += 5.0f;
-        t.v2.z += 5.0f;
-        t.v3.z += 5.0f;
-    }
-    Vector3d pivot = tris[10].v2;
 
-    //end of little testing bit
-
+    
+    
 
     bool running = true;
     SDL_Event event;
@@ -246,27 +335,35 @@ int main() {
 
 
         // Clear screen with a color (red, green, blue, alpha)
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); 
         SDL_RenderClear(renderer);
+        screenBuffer.fill(0);
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        int c = 0;
+
         for(Triangle t: tris){
             Vector2di p1, p2, p3;
-            p1 = orthos(t.v1, mainCamera);
-            p2 = orthos(t.v2, mainCamera);
-            p3 = orthos(t.v3, mainCamera);
+            p1 = orthos(t.v1, mainCamera, screenBuffer);
+            p2 = orthos(t.v2, mainCamera, screenBuffer);
+            p3 = orthos(t.v3, mainCamera, screenBuffer);
 
-            SDL_RenderLine(renderer, p1.x, p1.y, p2.x, p2.y);
-            SDL_RenderLine(renderer, p2.x, p2.y, p3.x, p3.y);
-            SDL_RenderLine(renderer, p3.x, p3.y, p1.x, p1.y);
+            screenBuffer.line(p1.x, p1.y, p2.x, p2.y, 0xFFFFFFFF);
+            screenBuffer.line(p2.x, p2.y, p3.x, p3.y, 0xFFFFFFFF);
+            screenBuffer.line(p3.x, p3.y, p1.x, p1.y, 0xFFFFFFFF);
 
+            SDL_Log(std::to_string(c).c_str());
+            c++;
         }
         
 
+
         mainCamera.rotation.y += 0.005f;
         // Present the frame
-        SDL_RenderPresent(renderer);
+        SDL_UpdateTexture(framebuffer, nullptr, screenBuffer.buffer.data(), screenBuffer.width * sizeof(uint32_t));
 
+        // render
+        SDL_RenderClear(renderer);
+        SDL_RenderTexture(renderer, framebuffer, nullptr, nullptr);
+        SDL_RenderPresent(renderer);
 
 	    Uint64 end = SDL_GetPerformanceCounter();
 
